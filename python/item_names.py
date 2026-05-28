4f7346a8dcaaf86db5f57f7dc75c0dfd1bb43e20
Geethu <geethu.suresh@version1.com>
2026-05-28 18:29:36 +0100

    Added enum for items

---
+++ python/item_names.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ItemName(str, Enum):
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    CONJURED_PREFIX = "Conjured"
